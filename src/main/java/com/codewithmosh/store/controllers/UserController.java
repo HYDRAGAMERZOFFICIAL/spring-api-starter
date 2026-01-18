14d0571b4096ce52320b935453ed80321bcfceb5
HYDRAGAMERZOFFICIAL <developer@smartlib.app>
2026-01-18 16:48:14 +0530

    DTO caling

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -1,11 +1,13 @@
 package com.codewithmosh.store.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.Mapper.UserMapper;
+import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.repositories.UserRepository;
 
 import lombok.AllArgsConstructor;
@@ -13,17 +15,34 @@ import lombok.AllArgsConstructor;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/users")
+
 public class UserController {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    @GetMapping("")
-    public Iterable<User> getallUsers() {
-        return userRepository.findAll();
+    @GetMapping
+    public Iterable<UserDto> getallUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(userMapper::toDto)
+                .toList();
     }
 
     @GetMapping("/{id}")
-    public User getUser(@PathVariable Long id) {
-        return userRepository.findById(id).orElse(null);
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
+        var user = userRepository.findById(id).orElse(null);
+
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+        return ResponseEntity.ok(userDto);
     }
 
 }
+
+// @GetMapping("/{id}")
+// public User getUser(@PathVariable Long id) {
+// return userRepository.findById(id).orElse(null);
+// }
